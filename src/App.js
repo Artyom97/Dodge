1d78448b4a1320bb31c1c37856834be9a46a1ed2
arantes15 <artyom@tidepoollabs.com>
2020-04-11 18:19:05 +0400

    favicon icon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header/Header";
 import Main from './components/Main/Main';
 import Footer from "./components/Footer/Footer";
 import './App.scss';
 
-class App extends Component {
-    render () {
-        return (
-            <div className="App">
-                <Header/>
-                <Main/>
-                <Footer/>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Main />
+            <Footer/>
+       </div>
+    );
 }
 
-export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,33 @@
-import React, {Component} from "react";
+import React from "react";
 import './Header.scss';
 import {NavLink} from "react-router-dom";
 
-class Header extends Component {
-    render () {
-        return (
-          <div className='Header'>
-             <div className='Header-block'>
-                <div className='Header-block__image'>
-                   <a href='#'>DODGE</a>
-                </div>
-                <div className='Header-block__ul'>
-                    <ul>
-                        <li>
-                            <NavLink to='/charger'>Charger</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/viper'>Viper</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/durango'>Durango</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to='/challenger'>Challenger</NavLink>
-                        </li>
-                    </ul>
-                </div>
-             </div>
-          </div>
-        )
-    }
+function Header () {
+    return (
+      <div className='Header'>
+         <div className='Header-block'>
+            <div className='Header-block__image'>
+                <NavLink to='/'>DODGE</NavLink>
+            </div>
+            <div className='Header-block__ul'>
+                <ul>
+                    <li>
+                        <NavLink to='/charger'>Charger</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/viper'>Viper</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/durango'>Durango</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/challenger'>Challenger</NavLink>
+                    </li>
+                </ul>
+            </div>
+         </div>
+      </div>
+    )
 }
 
 export default Header
