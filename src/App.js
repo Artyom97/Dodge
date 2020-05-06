9c0fbfc3c8f1d245e056d17589d12bf8f755b36f
arantes15 <artyom@tidepoollabs.com>
2020-05-07 02:38:41 +0400

    df

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             <CardHistories />
             <BrothersInfo />
             <Footer/>
-       </div> //changed 
+       </div> //changed sdsdsdsdssdsd
     );
 }
 
